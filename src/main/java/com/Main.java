194bacd8dee415089542afcac865de780f4864d4
leonid13171996 <leonid13171996@outlook.com>
2018-03-12 02:33:59 +0300

    setting run for herocu

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -2,13 +2,20 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-public class Main {
+public class Main extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Main.class);
+    }
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Main.class, args);
     }
