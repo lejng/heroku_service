9c663f7dc270b8de5ae6b68b531442e33ba7fdbc
Mishuk, Leonid <l.mishuk@a1qa.com>
2018-03-29 12:29:24 +0300

    refactoring

--- src/main/java/test/TestControllerAction.java
+++ src/main/java/test/TestControllerAction.java
@@ -3,9 +3,10 @@ package test;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class TestControllerActon {
-    private static final String LOGIN_URL = "https://service228.herokuapp.com/login";
-    private static final String ACCOUNT_URL = "https://service228.herokuapp.com/account";
+public class TestControllerAction {
+    private static final String HOST = "https://service228.herokuapp.com";//"http://localhost:8080/";
+    private static final String LOGIN_URL = HOST + "/login";
+    private static final String ACCOUNT_URL = HOST + "/account";
     private String phone = "+375293111716";
     private String password = "1234";
 
